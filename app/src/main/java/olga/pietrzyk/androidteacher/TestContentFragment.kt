053eca491d0dc52eaab8c34b4af742ccaa6ff31b
olga <olg262@gmail.com>
2020-02-08 21:55:24 +0100

    working test without passing a result

--- app/src/main/java/olga/pietrzyk/androidteacher/TestContentFragment.kt
+++ app/src/main/java/olga/pietrzyk/androidteacher/TestContentFragment.kt
@@ -2,11 +2,13 @@ package olga.pietrzyk.androidteacher
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import olga.pietrzyk.androidteacher.databinding.FragmentTestBinding
 import olga.pietrzyk.androidteacher.databinding.FragmentTestContentBinding
 
@@ -30,21 +32,72 @@ class TestContentFragment : Fragment() {
 
     lateinit var currentQuestion: TestQuestion
     lateinit var answers: MutableList<String>
-    val numOfQuestions = 3
+    val numberOfQuestions = 3
+    var numberOfCorrectAnswers=0
+    var finalResult:Double=0.0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
 
         val binding = DataBindingUtil.inflate<FragmentTestContentBinding>(inflater, R.layout.fragment_test_content, container, false)
+        var indexOfQuestion=0
 
-        currentQuestion=TestQuestion(question="What id the most current version of Android",
-            answers=listOf("1","2","3","4"))
-        answers=currentQuestion.answers.toMutableList()
-
+        var numberOfQuestionToBeAnswered=numberOfQuestions
+        var idIndex = -1
         binding.test=this
+
+        currentQuestion = questions[indexOfQuestion]
+        answers = currentQuestion.answers.toMutableList()
+
+
+            binding.answerButton.setOnClickListener { view: View ->
+                var userAnswerId = binding.radioGroup.checkedRadioButtonId
+
+
+
+                Log.i("AAAAAAARESULT", "nie wiem czy tu jetstem tutaj${numberOfQuestions}")
+
+                if (userAnswerId == R.id.btnAAnswer) {
+                    idIndex = 0
+                    Log.i("AAAAAAARESULT", "kliknales${idIndex}")
+                } else if (userAnswerId == R.id.btnBAnswer) {
+                    idIndex = 1
+                } else if (userAnswerId == R.id.btnCAnswer) {
+                    idIndex = 2
+                } else if (userAnswerId == R.id.btnDAnswer) {
+                    idIndex = 3
+                }
+
+                if (idIndex == 0) {
+                numberOfCorrectAnswers += 1
+                    Log.i("AAAAAAARESULT", "dobra odp${numberOfCorrectAnswers}")
+            }
+                if(idIndex!=-1) {
+                    numberOfQuestionToBeAnswered -= 1
+                    indexOfQuestion += 1
+                    Log.i("AAAAAAARESULT", "tu tez bylem ale nic nie zrobilem")
+                    binding.invalidateAll()
+                }
+
+                if(numberOfQuestionToBeAnswered==0){
+                    finalResult=(numberOfCorrectAnswers.toDouble()*100/numberOfQuestions.toDouble())
+                    Log.i("AAAAAAARESULT","L poprawnych odp ${numberOfCorrectAnswers} l odp to ${numberOfQuestions} wiec twój wynik to${finalResult}")
+                    Navigation.findNavController(view).navigate(R.id.action_testContentFragment_to_testResultFragment)
+                }
+
+                currentQuestion = questions[indexOfQuestion]
+                answers = currentQuestion.answers.toMutableList()
+            }
+
+
+
+
+
+
 
         return binding.root
         //return inflater.inflate(R.layout.fragment_test_content, container, false)
