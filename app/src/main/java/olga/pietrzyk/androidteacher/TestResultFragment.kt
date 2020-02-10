374c3dafe7959fe6e177f1c64c8b9c2233a7a88c
olga <olg262@gmail.com>
2020-02-10 03:37:40 +0100

    added sharing of the final result in the test

--- app/src/main/java/olga/pietrzyk/androidteacher/TestResultFragment.kt
+++ app/src/main/java/olga/pietrzyk/androidteacher/TestResultFragment.kt
@@ -1,11 +1,11 @@
 package olga.pietrzyk.androidteacher
 
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
@@ -14,7 +14,7 @@ import olga.pietrzyk.androidteacher.databinding.FragmentTestResultBinding
 
 /**
  * A simple [Fragment] subclass.
- */
+ */var result : String="I want to share with you my Android test result which is: "
 class TestResultFragment : Fragment() {
 
     override fun onCreateView(
@@ -23,17 +23,46 @@ class TestResultFragment : Fragment() {
     ): View? {
 
         val binding: FragmentTestResultBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_test_result, container, false)
-        //(activity as AppCompatActivity).supportActionBar?.title = "Test result"
         val testResultFragmentArgs by navArgs<TestResultFragmentArgs>()
+
         binding.finalResultText.text = testResultFragmentArgs.finalResult.toString()
 
+        result+=testResultFragmentArgs.finalResult.toString()+"%"
         binding.btnTryAgain.setOnClickListener { view:View ->
             Navigation.findNavController(view).navigate(R.id.action_testResultFragment_to_testFragment)
         }
+
+        setHasOptionsMenu(true)
         return binding.root
 
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.result_menu, menu)
+    }
 
+
+    private fun getShareIntent(): Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,
+
+                result
+                )
+        return shareIntent
+    }
+
+    private fun shareResult(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            R.id.resultMenu -> shareResult()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
